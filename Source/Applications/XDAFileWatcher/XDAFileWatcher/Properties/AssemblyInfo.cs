d02cd93cab4be032b8a00ad5f85df6941afba2eb
StephenCWills <swills@gridprotectionalliance.org>
2013-06-18 13:14:05 +0000

    Modified File Watcher configuration and assembly info.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openXDA/Main;C85419

--- Source/Applications/XDAFileWatcher/XDAFileWatcher/Properties/AssemblyInfo.cs
+++ Source/Applications/XDAFileWatcher/XDAFileWatcher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("openEDA File Watcher")]
+[assembly: AssemblyTitle("openXDA File Watcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Grid Protection Alliance")]
-[assembly: AssemblyProduct("openEDA File Watcher")]
+[assembly: AssemblyProduct("openXDA File Watcher")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
