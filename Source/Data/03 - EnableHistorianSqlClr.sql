031313b309f210d0e5ab7d4f3ac514f955bc4321
StephenCWills <swills@gridprotectionalliance.org>
2016-02-08 16:05:05 -0500

    Removed the default value from the channelIDs parameter on the ExternalGetTrendingData function of the SqlClr interface.

--- Source/Data/03 - EnableHistorianSqlClr.sql
+++ Source/Data/03 - EnableHistorianSqlClr.sql
@@ -5,7 +5,7 @@ RECONFIGURE
 GO
 
 -- Queries measurement data from the openHistorian
-CREATE FUNCTION GetHistorianData(@historianServer nvarchar(256), @instanceName nvarchar(256), @startTime datetime2, @stopTime datetime2, @measurementIDs nvarchar(MAX) = null)
+CREATE FUNCTION GetHistorianData(@historianServer nvarchar(256), @instanceName nvarchar(256), @startTime datetime2, @stopTime datetime2, @measurementIDs nvarchar(MAX))
 RETURNS TABLE
 (
    [ID] bigint,
@@ -33,8 +33,8 @@ RETURNS bigint
 AS EXTERNAL NAME [openHistorian.SqlClr].HistorianFunctions.MakeQuadWord;
 GO
 
--- Queries trending data from the openHistorian, example: "SELECT * FROM GetTrendingData('127.0.0.1', 'XDA', '2015-05-04 00:00:00', '2015-05-04 00:10:00', '11,21,31', default)"
-CREATE FUNCTION ExternalGetTrendingData(@historianServer nvarchar(256), @instanceName nvarchar(256), @startTime datetime2, @stopTime datetime2, @channelIDs nvarchar(MAX) = null, @seriesCount int = 3)
+-- Queries trending data from the openHistorian, example: "SELECT * FROM ExternalGetTrendingData('127.0.0.1', 'XDA', '2015-05-04 00:00:00', '2015-05-04 00:10:00', '11,21,31', default)"
+CREATE FUNCTION ExternalGetTrendingData(@historianServer nvarchar(256), @instanceName nvarchar(256), @startTime datetime2, @stopTime datetime2, @channelIDs nvarchar(MAX), @seriesCount int = 3)
 RETURNS TABLE
 (
    [ChannelID] int,
@@ -45,6 +45,7 @@ RETURNS TABLE
 AS EXTERNAL NAME [openHistorian.XDALink.SqlClr].XDAFunctions.GetTrendingData;
 GO
 
+-- Queries trending data from the openHistorian, example: "SELECT * FROM GetTrendingData('2015-05-04 00:00:00', '2015-05-04 00:10:00', '11,21,31', default)"
 CREATE FUNCTION GetTrendingData(@startTime datetime2, @stopTime datetime2, @channelIDs nvarchar(MAX) = null, @seriesCount int = 3)
 RETURNS @trendingData TABLE
 (
