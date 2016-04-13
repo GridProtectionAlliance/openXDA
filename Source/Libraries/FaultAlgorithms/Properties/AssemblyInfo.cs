bcb4d37d8f6f9386de48b76c34a56cdb47796947
gsfbuildbot <support@gridprotectionalliance.org>
2016-04-13 01:13:43 -0400

    openXDA: Version change for build v1.5.1.58-master.

--- Source/Libraries/FaultAlgorithms/Properties/AssemblyInfo.cs
+++ Source/Libraries/FaultAlgorithms/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.1.57")]
-[assembly: AssemblyVersion("1.5.1.57")]
-[assembly: AssemblyFileVersion("1.5.1.57")]
+// [assembly: AssemblyVersion("1.5.1.58")]
+[assembly: AssemblyVersion("1.5.1.58")]
+[assembly: AssemblyFileVersion("1.5.1.58")]
