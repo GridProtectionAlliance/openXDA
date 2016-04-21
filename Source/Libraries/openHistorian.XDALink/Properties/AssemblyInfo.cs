48b10c0e8e31ac32f701844cf8d6852c0a32d586
gsfbuildbot <support@gridprotectionalliance.org>
2016-04-21 00:41:24 -0400

    openXDA: Version change for build v1.5.1.65-master.

--- Source/Libraries/openHistorian.XDALink/Properties/AssemblyInfo.cs
+++ Source/Libraries/openHistorian.XDALink/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.1.64")]
-[assembly: AssemblyVersion("1.5.1.64")]
-[assembly: AssemblyFileVersion("1.5.1.64")]
+// [assembly: AssemblyVersion("1.5.1.65")]
+[assembly: AssemblyVersion("1.5.1.65")]
+[assembly: AssemblyFileVersion("1.5.1.65")]
