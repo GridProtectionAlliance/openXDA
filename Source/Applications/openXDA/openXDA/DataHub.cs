70ab80124fa94713104c6f0e0af73df12b02bbb9
Billy Ernest <bernest@gridprotectionalliance.org>
2016-06-17 13:43:33 -0400

    Added web based settings to openXDA

---
+++ Source/Applications/openXDA/openXDA/DataHub.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GSF.Data.Model;
+using GSF.Web.Model;
+using GSF.Web.Security;
+using Microsoft.AspNet.SignalR;
+using openXDA.Model;
+
+namespace openXDA
+{
+    public class DataHub: Hub, IRecordOperationsHub
+    {
+        #region [ Members ]
+
+        // Fields
+        private readonly DataContext m_dataContext;
+        private bool m_disposed;
+
+        #endregion
+
+        #region [ Constructors ]
+
+        public DataHub()
+        {
+            m_dataContext = new DataContext();
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets <see cref="IRecordOperationsHub.RecordOperationsCache"/> for SignalR hub.
+        /// </summary>
+        public RecordOperationsCache RecordOperationsCache => s_recordOperationsCache;
+
+        // Gets reference to MiPlan context, creating it if needed
+
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Releases the unmanaged resources used by the <see cref="DataHub"/> object and optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (!m_disposed)
+            {
+                try
+                {
+                    if (disposing)
+                        m_dataContext?.Dispose();
+                }
+                finally
+                {
+                    m_disposed = true;          // Prevent duplicate dispose.
+                    base.Dispose(disposing);    // Call base class Dispose().
+                }
+            }
+        }
+
+        public override Task OnConnected()
+        {
+            // Store the current connection ID for this thread
+            s_connectionID.Value = Context.ConnectionId;
+            s_connectCount++;
+
+            //MvcApplication.LogStatusMessage($"DataHub connect by {Context.User?.Identity?.Name ?? "Undefined User"} [{Context.ConnectionId}] - count = {s_connectCount}");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            if (stopCalled)
+            {
+                s_connectCount--;
+                //MvcApplication.LogStatusMessage($"DataHub disconnect by {Context.User?.Identity?.Name ?? "Undefined User"} [{Context.ConnectionId}] - count = {s_connectCount}");
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Properties
+
+        /// <summary>
+        /// Gets the hub connection ID for the current thread.
+        /// </summary>
+        public static string CurrentConnectionID => s_connectionID.Value;
+
+        // Static Fields
+        private static volatile int s_connectCount;
+        private static readonly ThreadLocal<string> s_connectionID = new ThreadLocal<string>();
+        private static readonly RecordOperationsCache s_recordOperationsCache;
+
+        /// <summary>
+        /// Gets statically cached instance of <see cref="RecordOperationsCache"/> for <see cref="DataHub"/> instances.
+        /// </summary>
+        /// <returns>Statically cached instance of <see cref="RecordOperationsCache"/> for <see cref="DataHub"/> instances.</returns>
+        public static RecordOperationsCache GetRecordOperationsCache() => s_recordOperationsCache;
+
+
+        // Static Constructor
+        static DataHub()
+        {
+            // Analyze and cache record operations of security hub
+            s_recordOperationsCache = new RecordOperationsCache(typeof(DataHub));
+        }
+
+        #endregion
+
+        [AuthorizeHubRole("Administrator")]
+        [RecordOperation(typeof(Setting), RecordOperation.QueryRecordCount)]
+        public int QuerySettingCount(string filterString)
+        {
+            return m_dataContext.Table<Setting>().QueryRecordCount();
+        }
+
+        [AuthorizeHubRole("Administrator")]
+        [RecordOperation(typeof(Setting), RecordOperation.QueryRecords)]
+        public IEnumerable<Setting> QuerySettings(string sortField, bool ascending, int page, int pageSize, string filterString)
+        {
+            return m_dataContext.Table<Setting>().QueryRecords(sortField, ascending, page, pageSize);
+        }
+
+        [AuthorizeHubRole("Administrator")]
+        [RecordOperation(typeof(Setting), RecordOperation.DeleteRecord)]
+        public void DeleteSetting(int id)
+        {
+            m_dataContext.Table<Setting>().DeleteRecord(id);
+        }
+
+        [AuthorizeHubRole("Administrator")]
+        [RecordOperation(typeof(Setting), RecordOperation.CreateNewRecord)]
+        public Setting NewActionItem()
+        {
+            return new Setting();
+        }
+
+        [AuthorizeHubRole("Administrator")]
+        [RecordOperation(typeof(Setting), RecordOperation.AddNewRecord)]
+        public void AddNewActionItem(Setting record)
+        {
+            m_dataContext.Table<Setting>().AddNewRecord(record);
+        }
+
+        [AuthorizeHubRole("Administrator, Owner")]
+        [RecordOperation(typeof(Setting), RecordOperation.UpdateRecord)]
+        public void UpdateActionItem(Setting record)
+        {
+            m_dataContext.Table<Setting>().UpdateRecord(record);
+        }
+
+
+    }
+}
---
+++ Source/Applications/openXDA/openXDA/GlobalSettings.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openXDA
+{
+    public class GlobalSettings
+    {
+        public string CompanyName
+        {
+            get;
+            set;
+        }
+
+        public string CompanyAcronym
+        {
+            get;
+            set;
+        }
+
+        public Guid NodeID
+        {
+            get;
+            set;
+        }
+
+        public string ApplicationName
+        {
+            get;
+            set;
+        }
+
+        public string ApplicationDescription
+        {
+            get;
+            set;
+        }
+
+        public string ApplicationKeywords
+        {
+            get;
+            set;
+        }
+
+        public string DateFormat
+        {
+            get;
+            set;
+        }
+
+        public string TimeFormat
+        {
+            get;
+            set;
+        }
+
+        public string DateTimeFormat
+        {
+            get;
+            set;
+        }
+
+        public string PasswordRequirementsRegex
+        {
+            get;
+            set;
+        }
+
+        public string PasswordRequirementsError
+        {
+            get;
+            set;
+        }
+
+        public string BootstrapTheme
+        {
+            get;
+            set;
+        }
+
+        public int DefaultDialUpRetries
+        {
+            get;
+            set;
+        }
+
+        public int DefaultDialUpTimeout
+        {
+            get;
+            set;
+        }
+
+        public string DefaultFTPUserName
+        {
+            get;
+            set;
+        }
+
+        public string DefaultFTPPassword
+        {
+            get;
+            set;
+        }
+
+        public string DefaultRemotePath
+        {
+            get;
+            set;
+        }
+
+        public string DefaultLocalPath
+        {
+            get;
+            set;
+        }
+
+    }
+}
