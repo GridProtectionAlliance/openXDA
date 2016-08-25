aded980448becd4868404c611a0cf479c9a7824e
gsfbuildbot <support@gridprotectionalliance.org>
2016-08-25 00:22:30 -0400

    openXDA: Version change for build v1.5.2.44-master.

--- Source/Applications/XDAFileWatcher/XDAFileWatcher/Properties/AssemblyInfo.cs
+++ Source/Applications/XDAFileWatcher/XDAFileWatcher/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.2.43")]
-[assembly: AssemblyVersion("1.5.2.43")]
-[assembly: AssemblyFileVersion("1.5.2.43")]
+// [assembly: AssemblyVersion("1.5.2.44")]
+[assembly: AssemblyVersion("1.5.2.44")]
+[assembly: AssemblyFileVersion("1.5.2.44")]
