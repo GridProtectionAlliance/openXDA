70225e8ab73b32cb9186d4b894e383ae506bc451
gsfbuildbot <support@gridprotectionalliance.org>
2016-09-14 00:36:29 -0400

    openXDA: Version change for build v1.5.2.60-master.

--- Source/Libraries/SqlServerTedWriter/Properties/AssemblyInfo.cs
+++ Source/Libraries/SqlServerTedWriter/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.2.59")]
-[assembly: AssemblyVersion("1.5.2.59")]
-[assembly: AssemblyFileVersion("1.5.2.59")]
+// [assembly: AssemblyVersion("1.5.2.60")]
+[assembly: AssemblyVersion("1.5.2.60")]
+[assembly: AssemblyFileVersion("1.5.2.60")]
