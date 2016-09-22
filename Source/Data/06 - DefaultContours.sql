0b164e7ae639939ce6c83f75a8e8d7ce9eb89dab
StephenCWills <swills@gridprotectionalliance.org>
2016-09-22 12:55:59 -0400

    Added default contour scales for voltage THD and flicker.

--- Source/Data/06 - DefaultContours.sql
+++ Source/Data/06 - DefaultContours.sql
@@ -32,3 +32,153 @@ SELECT 1 ContourColorScaleID,     2 Value, 0xAAFFFF00 Color, 20 OrderID UNION AL
 SELECT 1 ContourColorScaleID,     2 Value, 0xAAFF0000 Color, 21 OrderID UNION ALL
 SELECT 1 ContourColorScaleID,     3 Value, 0xAAFF0000 Color, 22 OrderID
 GO
+
+INSERT INTO ContourColorScale(Name, NominalValue)
+VALUES('Voltage THD (L-G)', 0.0)
+GO
+
+INSERT INTO ContourChannelType(ContourColorScaleID, MeasurementTypeID, MeasurementCharacteristicID, PhaseID)
+SELECT 2 ContourColorScaleID, MeasurementType.ID MeasurementTypeID, MeasurementCharacteristic.ID MeasurementCharacteristicID, Phase.ID PhaseID
+FROM MeasurementType CROSS JOIN MeasurementCharacteristic CROSS JOIN Phase
+WHERE MeasurementType.Name = 'Voltage' AND MeasurementCharacteristic.Name = 'TotalTHD' AND Phase.Name IN ('AN', 'BN', 'CN')
+GO
+
+INSERT INTO ContourColorScalePoint(ContourColorScaleID, Value, Color, OrderID)
+SELECT 2 ContourColorScaleID, 0 Value, 0x00000000 Color,  1 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 1 Value, 0x00000000 Color,  2 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 1 Value, 0xAA0000FF Color,  3 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 2 Value, 0xAA0000FF Color,  4 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 2 Value, 0xAA00FFFF Color,  5 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 3 Value, 0xAA00FFFF Color,  6 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 3 Value, 0xAA00FF00 Color,  7 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 4 Value, 0xAA00FF00 Color,  8 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 4 Value, 0xAAFFFF00 Color,  9 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 5 Value, 0xAAFFFF00 Color, 10 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 5 Value, 0xAAFF0000 Color, 11 OrderID UNION ALL
+SELECT 2 ContourColorScaleID, 6 Value, 0xAAFF0000 Color, 12 OrderID
+GO
+
+INSERT INTO ContourColorScale(Name, NominalValue)
+VALUES('Voltage THD (AN)', 0.0)
+GO
+
+INSERT INTO ContourChannelType(ContourColorScaleID, MeasurementTypeID, MeasurementCharacteristicID, PhaseID)
+SELECT 3 ContourColorScaleID, MeasurementType.ID MeasurementTypeID, MeasurementCharacteristic.ID MeasurementCharacteristicID, Phase.ID PhaseID
+FROM MeasurementType CROSS JOIN MeasurementCharacteristic CROSS JOIN Phase
+WHERE MeasurementType.Name = 'Voltage' AND MeasurementCharacteristic.Name = 'TotalTHD' AND Phase.Name IN ('AN')
+GO
+
+INSERT INTO ContourColorScalePoint(ContourColorScaleID, Value, Color, OrderID)
+SELECT 3 ContourColorScaleID, 0 Value, 0x00000000 Color,  1 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 1 Value, 0x00000000 Color,  2 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 1 Value, 0xAA0000FF Color,  3 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 2 Value, 0xAA0000FF Color,  4 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 2 Value, 0xAA00FFFF Color,  5 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 3 Value, 0xAA00FFFF Color,  6 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 3 Value, 0xAA00FF00 Color,  7 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 4 Value, 0xAA00FF00 Color,  8 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 4 Value, 0xAAFFFF00 Color,  9 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 5 Value, 0xAAFFFF00 Color, 10 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 5 Value, 0xAAFF0000 Color, 11 OrderID UNION ALL
+SELECT 3 ContourColorScaleID, 6 Value, 0xAAFF0000 Color, 12 OrderID
+GO
+
+INSERT INTO ContourColorScale(Name, NominalValue)
+VALUES('Voltage THD (BN)', 0.0)
+GO
+
+INSERT INTO ContourChannelType(ContourColorScaleID, MeasurementTypeID, MeasurementCharacteristicID, PhaseID)
+SELECT 4 ContourColorScaleID, MeasurementType.ID MeasurementTypeID, MeasurementCharacteristic.ID MeasurementCharacteristicID, Phase.ID PhaseID
+FROM MeasurementType CROSS JOIN MeasurementCharacteristic CROSS JOIN Phase
+WHERE MeasurementType.Name = 'Voltage' AND MeasurementCharacteristic.Name = 'TotalTHD' AND Phase.Name IN ('BN')
+GO
+
+INSERT INTO ContourColorScalePoint(ContourColorScaleID, Value, Color, OrderID)
+SELECT 4 ContourColorScaleID, 0 Value, 0x00000000 Color,  1 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 1 Value, 0x00000000 Color,  2 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 1 Value, 0xAA0000FF Color,  3 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 2 Value, 0xAA0000FF Color,  4 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 2 Value, 0xAA00FFFF Color,  5 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 3 Value, 0xAA00FFFF Color,  6 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 3 Value, 0xAA00FF00 Color,  7 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 4 Value, 0xAA00FF00 Color,  8 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 4 Value, 0xAAFFFF00 Color,  9 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 5 Value, 0xAAFFFF00 Color, 10 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 5 Value, 0xAAFF0000 Color, 11 OrderID UNION ALL
+SELECT 4 ContourColorScaleID, 6 Value, 0xAAFF0000 Color, 12 OrderID
+GO
+
+INSERT INTO ContourColorScale(Name, NominalValue)
+VALUES('Voltage THD (CN)', 0.0)
+GO
+
+INSERT INTO ContourChannelType(ContourColorScaleID, MeasurementTypeID, MeasurementCharacteristicID, PhaseID)
+SELECT 5 ContourColorScaleID, MeasurementType.ID MeasurementTypeID, MeasurementCharacteristic.ID MeasurementCharacteristicID, Phase.ID PhaseID
+FROM MeasurementType CROSS JOIN MeasurementCharacteristic CROSS JOIN Phase
+WHERE MeasurementType.Name = 'Voltage' AND MeasurementCharacteristic.Name = 'TotalTHD' AND Phase.Name IN ('CN')
+GO
+
+INSERT INTO ContourColorScalePoint(ContourColorScaleID, Value, Color, OrderID)
+SELECT 5 ContourColorScaleID, 0 Value, 0x00000000 Color,  1 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 1 Value, 0x00000000 Color,  2 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 1 Value, 0xAA0000FF Color,  3 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 2 Value, 0xAA0000FF Color,  4 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 2 Value, 0xAA00FFFF Color,  5 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 3 Value, 0xAA00FFFF Color,  6 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 3 Value, 0xAA00FF00 Color,  7 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 4 Value, 0xAA00FF00 Color,  8 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 4 Value, 0xAAFFFF00 Color,  9 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 5 Value, 0xAAFFFF00 Color, 10 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 5 Value, 0xAAFF0000 Color, 11 OrderID UNION ALL
+SELECT 5 ContourColorScaleID, 6 Value, 0xAAFF0000 Color, 12 OrderID
+GO
+
+INSERT INTO ContourColorScale(Name, NominalValue)
+VALUES('Flicker PST', 0.0)
+GO
+
+INSERT INTO ContourChannelType(ContourColorScaleID, MeasurementTypeID, MeasurementCharacteristicID, PhaseID)
+SELECT 6 ContourColorScaleID, MeasurementType.ID MeasurementTypeID, MeasurementCharacteristic.ID MeasurementCharacteristicID, Phase.ID PhaseID
+FROM MeasurementType CROSS JOIN MeasurementCharacteristic CROSS JOIN Phase
+WHERE MeasurementType.Name = 'Voltage' AND MeasurementCharacteristic.Name = 'FlkrPST' AND Phase.Name IN ('AN', 'BN', 'CN')
+GO
+
+INSERT INTO ContourColorScalePoint(ContourColorScaleID, Value, Color, OrderID)
+SELECT 6 ContourColorScaleID,   0 Value, 0x00000000 Color,  1 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.2 Value, 0x00000000 Color,  2 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.2 Value, 0xAA0000FF Color,  3 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.4 Value, 0xAA0000FF Color,  4 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.4 Value, 0xAA00FFFF Color,  5 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.6 Value, 0xAA00FFFF Color,  6 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.6 Value, 0xAA00FF00 Color,  7 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.8 Value, 0xAA00FF00 Color,  8 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 0.8 Value, 0xAAFFFF00 Color,  9 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 1.0 Value, 0xAAFFFF00 Color, 10 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 1.0 Value, 0xAAFF0000 Color, 11 OrderID UNION ALL
+SELECT 6 ContourColorScaleID, 1.2 Value, 0xAAFF0000 Color, 12 OrderID
+GO
+
+INSERT INTO ContourColorScale(Name, NominalValue)
+VALUES('Flicker PLT', 0.0)
+GO
+
+INSERT INTO ContourChannelType(ContourColorScaleID, MeasurementTypeID, MeasurementCharacteristicID, PhaseID)
+SELECT 7 ContourColorScaleID, MeasurementType.ID MeasurementTypeID, MeasurementCharacteristic.ID MeasurementCharacteristicID, Phase.ID PhaseID
+FROM MeasurementType CROSS JOIN MeasurementCharacteristic CROSS JOIN Phase
+WHERE MeasurementType.Name = 'Voltage' AND MeasurementCharacteristic.Name = 'FlkrPST' AND Phase.Name IN ('AN', 'BN', 'CN')
+GO
+
+INSERT INTO ContourColorScalePoint(ContourColorScaleID, Value, Color, OrderID)
+SELECT 7 ContourColorScaleID,    0 Value, 0x00000000 Color,  1 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.16 Value, 0x00000000 Color,  2 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.16 Value, 0xAA0000FF Color,  3 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.32 Value, 0xAA0000FF Color,  4 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.32 Value, 0xAA00FFFF Color,  5 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.48 Value, 0xAA00FFFF Color,  6 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.48 Value, 0xAA00FF00 Color,  7 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.64 Value, 0xAA00FF00 Color,  8 OrderID UNION ALL
+SELECT 7 ContourColorScaleID, 0.64 Value, 0xAAFFFF00 Color,  9 OrderID UNION ALL
+SELECT 7 ContourColorScaleID,  0.8 Value, 0xAAFFFF00 Color, 10 OrderID UNION ALL
+SELECT 7 ContourColorScaleID,  0.8 Value, 0xAAFF0000 Color, 11 OrderID UNION ALL
+SELECT 7 ContourColorScaleID,  1.0 Value, 0xAAFF0000 Color, 12 OrderID
+GO
