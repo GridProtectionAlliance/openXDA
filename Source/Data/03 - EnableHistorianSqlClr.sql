98af7e6857d714af393d4ca032e673f1ddd9f5f6
StephenCWills <swills@gridprotectionalliance.org>
2016-09-30 10:44:46 -0400

    Added the openXDA.SqlClr.dll assembly as well as the GetEventData() and GetFaultData functions to the openXDA database via the WiX installer.

--- Source/Data/03 - EnableHistorianSqlClr.sql
+++ Source/Data/03 - EnableHistorianSqlClr.sql
@@ -113,3 +113,24 @@ BEGIN
 	RETURN
 END
 GO
+
+CREATE FUNCTION GetEventData(@eventID INT)
+RETURNS TABLE
+(
+    SeriesID INT,
+    Characteristic NVARCHAR(200),
+    Time DATETIME2,
+    Value FLOAT
+)
+AS EXTERNAL NAME [openXDA.SqlClr].[openXDA.SqlClr.XDAFunctions].GetEventData;
+GO
+
+CREATE FUNCTION GetFaultData(@eventID INT)
+RETURNS TABLE
+(
+    Algorithm NVARCHAR(80),
+    Time DATETIME2,
+    Value FLOAT
+)
+AS EXTERNAL NAME [openXDA.SqlClr].[openXDA.SqlClr.XDAFunctions].GetFaultData;
+GO
