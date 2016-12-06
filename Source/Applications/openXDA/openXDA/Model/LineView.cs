353e8da8784e72b2e0847217466b06a8c41370c0
Billy Ernest <bernest@gridprotectionalliance.org>
2016-12-06 13:45:22 -0500

    fixed lineview model to contain fields from line instead of inheriting line because it was screwing reflection up with the paged view model commands

--- Source/Applications/openXDA/openXDA/Model/LineView.cs
+++ Source/Applications/openXDA/openXDA/Model/LineView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,24 @@ using GSF.Data.Model;
 namespace openXDA.Model
 {
     [TableName("LineView")]
-    public class LineView: Line
+    public class LineView
     {
-        public string TopName { get; set; }
         [PrimaryKey(true)]
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        public string AssetKey { get; set; }
+
+        public float VoltageKV { get; set; }
+
+        public float ThermalRating { get; set; }
+
+        public float Length { get; set; }
+
+        public string Description { get; set; }
+
+        public string TopName { get; set; }
+
         public int LineImpedanceID { get; set; }
         public float R0 { get; set; }
         public float R1 { get; set; }
