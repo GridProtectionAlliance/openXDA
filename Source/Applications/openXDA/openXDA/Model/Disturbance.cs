aed78325a777d182cc585d1bc0f0101a66d2ea12
Billy Ernest <bernest@gridprotectionalliance.org>
2016-12-12 14:57:37 -0500

    Made severity code nullable in the disturbance view model

--- Source/Applications/openXDA/openXDA/Model/Disturbance.cs
+++ Source/Applications/openXDA/openXDA/Model/Disturbance.cs
@@ -23,10 +23,23 @@ namespace openXDA.Model
         public int EndIndex { get; set; }
     }
 
-    public class DisturbanceView : Disturbance
+    public class DisturbanceView
     {
+        [PrimaryKey(true)]
+        public int ID { get; set; }
+        public int EventID { get; set; }
+        public int EventTypeID { get; set; }
+        public int PhaseID { get; set; }
+        public float Magnitude { get; set; }
+        public float PerUnitMagnitude { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public float DurationSeconds { get; set; }
+        public float DurationCycles { get; set; }
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
         public int MeterID { get; set; }
-        public int SeverityCode { get; set; }
+        public int? SeverityCode { get; set; }
         public string MeterName { get; set; }
         public string PhaseName { get; set; }
 
