89a52e0ad99a001fa8df01d2ad1a2293c956a69e
StephenCWills <swills@gridprotectionalliance.org>
2017-01-19 10:42:43 -0500

    Tweaked database install to allow more user customization of database features.

---
+++ Source/Data/03 - EnableEventDataSqlClr.sql
@@ -0,0 +1,26 @@
+﻿EXEC sp_configure 'clr enabled', 1
+GO
+
+RECONFIGURE
+GO
+
+CREATE FUNCTION GetEventData(@eventID INT)
+RETURNS TABLE
+(
+    SeriesID INT,
+    Characteristic NVARCHAR(200),
+    Time DATETIME2,
+    Value FLOAT
+)
+AS EXTERNAL NAME [openXDA.SqlClr].[openXDA.SqlClr.XDAFunctions].GetEventData;
+GO
+
+CREATE FUNCTION GetFaultData(@eventID INT)
+RETURNS TABLE
+(
+    Algorithm NVARCHAR(80),
+    Time DATETIME2,
+    Value FLOAT
+)
+AS EXTERNAL NAME [openXDA.SqlClr].[openXDA.SqlClr.XDAFunctions].GetFaultData;
+GO
