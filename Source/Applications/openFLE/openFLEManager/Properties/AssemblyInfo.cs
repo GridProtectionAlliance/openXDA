c4a5953752bcef497a5fe1ea390b112445169365
gsfbuildbot <support@gridprotectionalliance.org>
2017-03-09 11:38:35 -0500

    openXDA: Version change for build v2.1.0.24-master.

--- Source/Applications/openFLE/openFLEManager/Properties/AssemblyInfo.cs
+++ Source/Applications/openFLE/openFLEManager/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.1.0.23")]
-[assembly: AssemblyVersion("2.1.0.23")]
-[assembly: AssemblyFileVersion("2.1.0.23")]
+// [assembly: AssemblyVersion("2.1.0.24")]
+[assembly: AssemblyVersion("2.1.0.24")]
+[assembly: AssemblyFileVersion("2.1.0.24")]
