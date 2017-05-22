a4bc3ab3c3004292ea2a82773948d3c38bd0dd2a
StephenCWills <swills@gridprotectionalliance.org>
2017-05-22 10:45:50 -0400

    Restored select list in selectSitesBreakersDetailsByDate stored procedure.

--- Source/Data/02 - DashSprocs.sql
+++ Source/Data/02 - DashSprocs.sql
@@ -2888,7 +2888,6 @@ GO
 -- [selectSitesBreakersDetailsByDate] '09/30/2014 06:43:57 AM', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,216,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,341,1340,1341,1342,1343,1344,1345,1346,1347,1349,1350,1351,1352,1353,1354,1355,1356,1357,1358,1359,1360,1363,1364,1365', 'S-1-5-21-4100281765-940110593-2650863885-4619', 'second'
 -- =============================================
 CREATE PROCEDURE [dbo].[selectSitesBreakersDetailsByDate]
-    -- Add the parameters for the stored procedure here
     @EventDate AS DATETIME,
     @MeterID AS NVARCHAR(MAX),
     @username AS NVARCHAR(4000),
@@ -2913,11 +2912,13 @@ BEGIN
         Meter.ID AS meterid, 
         Event.ID AS theeventid, 
         EventType.Name AS eventtype, 
+        BreakerOperation.ID AS breakeroperationid,
         CAST(CAST(BreakerOperation.TripCoilEnergized AS TIME) AS NVARCHAR(100)) AS energized,
         BreakerOperation.BreakerNumber AS breakernumber,
         MeterLine.LineName AS linename,
         Phase.Name AS phasename,
         CAST(BreakerOperation.BreakerTiming AS DECIMAL(16,5)) AS timing,
+        CAST(BreakerOperation.StatusTiming AS DECIMAL(16,5)) AS statustiming,
         BreakerOperation.BreakerSpeed AS speed,
         BreakerOperationType.Name AS operationtype 
     FROM
@@ -2933,7 +2934,6 @@ BEGIN
             TripCoilEnergized >= @startDate AND TripCoilEnergized < @endDate AND
             Meter.ID IN (SELECT * FROM  dbo.String_to_int_table(@MeterID, ',')) AND
 			Meter.ID IN (select * from authMeters(@username))
-
 END
 GO
 
