35a9a93043d2d8b1e5a547d9a435824fcb3a4ccf
StephenCWills <swills@gridprotectionalliance.org>
2019-01-29 11:02:31 -0500

    Fixed logic errors and exceptions in StatisticOperation.

--- Source/Libraries/FaultData/DataOperations/StatisticOperation.cs
+++ Source/Libraries/FaultData/DataOperations/StatisticOperation.cs
@@ -22,7 +22,9 @@
 //******************************************************************************************************
 
 
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using FaultData.DataAnalysis;
 using FaultData.DataResources;
@@ -38,64 +40,111 @@ namespace FaultData.DataOperations
         public override void Execute(MeterDataSet meterDataSet)
         {
             CycleDataResource cycleDataResource = meterDataSet.GetResource<CycleDataResource>();
-            SagDataResource sagDataResource = meterDataSet.GetResource<SagDataResource>();
-            SwellDataResource swellDataResource = meterDataSet.GetResource<SwellDataResource>();
-            InterruptionDataResource interruptionDataResource = meterDataSet.GetResource<InterruptionDataResource>();
             FaultDataResource faultDataResource = meterDataSet.GetResource<FaultDataResource>();
-            EventClassificationResource eventClassificationResource = meterDataSet.GetResource<EventClassificationResource>();
+
             using (AdoDataConnection connection = meterDataSet.CreateDbConnection())
             {
-                TableOperations<Event> eventTable = new TableOperations<Event>(connection);
-
-                List<Event> events = eventTable
-                    .QueryRecordsWhere("FileGroupID = {0}", meterDataSet.FileGroup.ID)
-                    .ToList();
-
-                foreach (Event evt in events)
+                object ToDateTime2(DateTime dateTime)
                 {
-                    // TODO: Calculate statistics here
-                    double? iMin = meterDataSet.DataSeries.Where(ds => ds.SeriesInfo.Channel.MeasurementType.Name == "Current" && ds.SeriesInfo.Channel.MeasurementCharacteristic.Name == "Instantaneous")?.Select(ds => ds.DataPoints.Where(dp => dp.Time >= evt.StartTime && dp.Time <= evt.EndTime).Min(dp => dp.Value)).Min() ?? null;
-                    double? iMax = meterDataSet.DataSeries.Where(ds => ds.SeriesInfo.Channel.MeasurementType.Name == "Current" && ds.SeriesInfo.Channel.MeasurementCharacteristic.Name == "Instantaneous")?.Select(ds => ds.DataPoints.Where(dp => dp.Time >= evt.StartTime && dp.Time <= evt.EndTime).Max(dp => dp.Value)).Max() ?? null;
-                    double? vMin = meterDataSet.DataSeries.Where(ds => ds.SeriesInfo.Channel.MeasurementType.Name == "Voltage" && ds.SeriesInfo.Channel.MeasurementCharacteristic.Name == "Instantaneous")?.Select(ds => ds.DataPoints.Where(dp => dp.Time >= evt.StartTime && dp.Time <= evt.EndTime).Min(dp => dp.Value)).Min() ?? null;
-                    double? vMax = meterDataSet.DataSeries.Where(ds => ds.SeriesInfo.Channel.MeasurementType.Name == "Voltage" && ds.SeriesInfo.Channel.MeasurementCharacteristic.Name == "Instantaneous")?.Select(ds => ds.DataPoints.Where(dp => dp.Time >= evt.StartTime && dp.Time <= evt.EndTime).Max(dp => dp.Value)).Max() ?? null;
-                    double? i2t = CalcI2t(meterDataSet, faultDataResource, cycleDataResource);
+                    using (IDbCommand command = connection.Connection.CreateCommand())
+                    {
+                        IDbDataParameter parameter = command.CreateParameter();
+                        parameter.DbType = DbType.DateTime2;
+                        parameter.Value = dateTime;
+                        return parameter;
+                    }
+                }
 
-                    // TODO: Push statistics to table here
-                    (new TableOperations<EventStat>(connection)).AddNewRecord(new EventStat() {
+                TableOperations<Event> eventTable = new TableOperations<Event>(connection);
+                TableOperations<EventStat> eventStatTable = new TableOperations<EventStat>(connection);
+
+                for (int i = 0; i < cycleDataResource.DataGroups.Count; i++)
+                {
+                    const string Filter =
+                        "FileGroupID = {0} AND " +
+                        "LineID = {1} AND " +
+                        "StartTime = {2} AND " +
+                        "EndTime = {3}";
+
+                    DataGroup dataGroup = cycleDataResource.DataGroups[i];
+                    VICycleDataGroup viCycleDataGroup = cycleDataResource.VICycleDataGroups[i];
+                    int fileGroupID = meterDataSet.FileGroup.ID;
+                    int lineID = dataGroup.Line.ID;
+                    object startTime = ToDateTime2(dataGroup.StartTime);
+                    object endTime = ToDateTime2(dataGroup.EndTime);
+
+                    Event evt = eventTable.QueryRecordWhere(Filter, fileGroupID, lineID, startTime, endTime);
+
+                    if (evt == null)
+                        continue;
+
+                    DataSeries[] currents = { viCycleDataGroup.IA?.RMS, viCycleDataGroup.IB?.RMS, viCycleDataGroup.IC?.RMS };
+                    DataSeries[] voltages = { viCycleDataGroup.VA?.RMS, viCycleDataGroup.VB?.RMS, viCycleDataGroup.VC?.RMS };
+
+                    if (voltages.All(voltage => voltage == null))
+                        voltages = new[] { viCycleDataGroup.VAB?.RMS, viCycleDataGroup.VBC?.RMS, viCycleDataGroup.VCA?.RMS };
+
+                    List<DataPoint> currentDataPoints = currents
+                        .Where(ds => ds != null)
+                        .SelectMany(ds => ds.DataPoints)
+                        .DefaultIfEmpty(null)
+                        .ToList();
+
+                    List<DataPoint> voltageDataPoints = voltages
+                        .Where(ds => ds != null)
+                        .SelectMany(ds => ds.DataPoints)
+                        .DefaultIfEmpty(null)
+                        .ToList();
+
+                    double? iMin = currentDataPoints.Min(dp => dp?.Value);
+                    double? iMax = currentDataPoints.Max(dp => dp?.Value);
+                    double? vMin = voltageDataPoints.Min(dp => dp?.Value);
+                    double? vMax = voltageDataPoints.Max(dp => dp?.Value);
+                    double? i2t = null;
+
+                    if (faultDataResource.FaultLookup.TryGetValue(dataGroup, out DataAnalysis.FaultGroup faultGroup))
+                        i2t = CalcI2t(faultGroup, viCycleDataGroup);
+
+                    eventStatTable.AddNewRecord(new EventStat()
+                    {
                         EventID = evt.ID,
                         IMax = iMax,
                         IMin = iMin,
                         VMax = vMax,
-                        VMin = vMin
+                        VMin = vMin,
+                        I2t = i2t
                     });
-
                 }
             }
         }
-        private double? CalcI2t(MeterDataSet meterDataSet, FaultDataResource faultDataResource, CycleDataResource cycleDataResource)
+
+        private double? CalcI2t(DataAnalysis.FaultGroup faultGroup, VICycleDataGroup viCycleDataGroup)
         {
             double i2t = 0;
-            foreach (KeyValuePair<DataGroup, DataAnalysis.FaultGroup> faultGroup in faultDataResource.FaultLookup)
+
+            foreach (DataAnalysis.Fault fault in faultGroup.Faults)
             {
-                foreach (DataAnalysis.Fault fault in faultGroup.Value.Faults)
+                if (fault.IsSuppressed)
+                    continue;
+
+                foreach (DataAnalysis.Fault.Summary faultSummary in fault.Summaries)
                 {
-                    if (fault.IsSuppressed) continue;
+                    if (!faultSummary.IsValid || !faultSummary.IsSelectedAlgorithm)
+                        continue;
 
-                    foreach (DataAnalysis.Fault.Summary faultSummary in fault.Summaries) {
-                        if (!faultSummary.IsValid || !faultSummary.IsSelectedAlgorithm) continue;
+                    double samplingInterval = 1.0D / viCycleDataGroup.IA.RMS.SampleRate;
 
-                         i2t += cycleDataResource
-                            .VICycleDataGroups.Select( vcdg => (new List<double>() {
-                                vcdg.IA.RMS.DataPoints.Where(dp => dp.Time >= fault.InceptionTime && dp.Time <= fault.ClearingTime).Select(dp => dp.Value * dp.Value).Sum(),
-                                vcdg.IB.RMS.DataPoints.Where(dp => dp.Time >= fault.InceptionTime && dp.Time <= fault.ClearingTime).Select(dp => dp.Value * dp.Value).Sum(),
-                                vcdg.IC.RMS.DataPoints.Where(dp => dp.Time >= fault.InceptionTime && dp.Time <= fault.ClearingTime).Select(dp => dp.Value * dp.Value).Sum() }).Sum()).Sum();
-                    }
+                    i2t += new[] { viCycleDataGroup.IA?.RMS, viCycleDataGroup.IB?.RMS, viCycleDataGroup.IC?.RMS }
+                        .Where(ds => ds != null)
+                        .SelectMany(ds => ds.DataPoints)
+                        .Where(dp => dp.Time >= fault.InceptionTime && dp.Time <= fault.ClearingTime)
+                        .Select(dp => dp.Value * dp.Value * samplingInterval)
+                        .Sum();
                 }
             }
 
             return (i2t != 0 ? i2t : (double?)null);
         }
-
     }
 
 }
