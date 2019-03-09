1550097f14c85e100e4c954dc1657f72b4be69ce
gsfbuildbot <support@gridprotectionalliance.org>
2019-03-09 00:30:02 -0500

    openXDA: Version change for build v2.3.2.109-master.

--- Source/Tools/XDAAlarmCreationApp/Properties/AssemblyInfo.cs
+++ Source/Tools/XDAAlarmCreationApp/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.2.108")]
-[assembly: AssemblyVersion("2.3.2.108")]
-[assembly: AssemblyFileVersion("2.3.2.108")]
+// [assembly: AssemblyVersion("2.3.2.109")]
+[assembly: AssemblyVersion("2.3.2.109")]
+[assembly: AssemblyFileVersion("2.3.2.109")]
