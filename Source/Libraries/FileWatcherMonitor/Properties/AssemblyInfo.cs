c50972103136b2054f0a91aeb6678dce791b7209
gsfbuildbot <support@gridprotectionalliance.org>
2019-04-26 00:23:29 -0400

    openXDA: Version change for build v2.3.2.147-master.

--- Source/Libraries/FileWatcherMonitor/Properties/AssemblyInfo.cs
+++ Source/Libraries/FileWatcherMonitor/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.2.146")]
-[assembly: AssemblyVersion("2.3.2.146")]
-[assembly: AssemblyFileVersion("2.3.2.146")]
+// [assembly: AssemblyVersion("2.3.2.147")]
+[assembly: AssemblyVersion("2.3.2.147")]
+[assembly: AssemblyFileVersion("2.3.2.147")]
