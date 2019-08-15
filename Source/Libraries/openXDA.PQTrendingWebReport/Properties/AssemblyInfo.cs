329c8139420517c27720897c2c8cb79ed0a58595
gsfbuildbot <support@gridprotectionalliance.org>
2019-08-15 00:03:03 -0400

    openXDA: Version change for build v2.4.0.53-master.

--- Source/Libraries/openXDA.PQTrendingWebReport/Properties/AssemblyInfo.cs
+++ Source/Libraries/openXDA.PQTrendingWebReport/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.4.0.52")]
-[assembly: AssemblyVersion("2.4.0.52")]
-[assembly: AssemblyFileVersion("2.4.0.52")]
+// [assembly: AssemblyVersion("2.4.0.53")]
+[assembly: AssemblyVersion("2.4.0.53")]
+[assembly: AssemblyFileVersion("2.4.0.53")]
