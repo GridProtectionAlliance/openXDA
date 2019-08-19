e12d7757c2ebe2b08ab182118d98e76b43126abc
StephenCWills <swills@gridprotectionalliance.org>
2019-08-19 17:06:21 -0400

    Added tab to event email parameters page to assist with testing changes to email templates.

---
+++ Source/Applications/openXDA/openXDA/Controllers/Config/EmailTemplateController.cs
@@ -0,0 +1,78 @@
+﻿//******************************************************************************************************
+//  EmailTemplateController.cs - Gbtc
+//
+//  Copyright © 2019, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  08/19/2019 - Stephen C. Wills
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using GSF.Data;
+using GSF.Data.Model;
+using openXDA.Model;
+
+namespace openXDA.Controllers.Config
+{
+    [RoutePrefix("api/Config/EmailTemplate")]
+    public class EmailTemplateController : ApiController
+    {
+        [Route("{templateID}"), HttpGet]
+        public List<EventView> Get(int templateID)
+        {
+            NameValueCollection queryParameters = Request.RequestUri.ParseQueryString();
+            string countValue = queryParameters["count"];
+
+            if (!int.TryParse(countValue, out int count))
+                count = 50;
+
+            using (AdoDataConnection connection = new AdoDataConnection("systemSettings"))
+            {
+                string getTriggerSQL =
+                    "SELECT TriggersEmailSQL " +
+                    "FROM " +
+                    "    EventEmailParameters JOIN " +
+                    "    EmailType ON EventEmailParameters.EmailTypeID = EmailType.ID " +
+                    "WHERE XSLTemplateID = {0}";
+
+                string triggerFormat = connection.ExecuteScalar<string>(getTriggerSQL, templateID);
+                string triggerSQL = string.Format(triggerFormat, "EventView.ID");
+
+                string getEvents =
+                    $"SELECT TOP {count} EventView.* " +
+                    $"FROM " +
+                    $"    EventView CROSS APPLY " +
+                    $"    ({triggerSQL}) EmailTrigger(Value) " +
+                    $"WHERE EmailTrigger.Value <> 0 " +
+                    $"ORDER BY EventView.StartTime DESC";
+
+                TableOperations<EventView> eventTable = new TableOperations<EventView>(connection);
+
+                return connection.RetrieveData(getEvents)
+                    .AsEnumerable()
+                    .Select(eventTable.LoadRecord)
+                    .ToList();
+            }
+
+        }
+    }
+}
