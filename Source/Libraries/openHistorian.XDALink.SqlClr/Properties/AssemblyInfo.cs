fac2d71bba429ca566961279e93fcdd95ff1674f
gsfbuildbot <support@gridprotectionalliance.org>
2019-09-10 00:02:51 -0400

    openXDA: Version change for build v2.4.0.72-master.

--- Source/Libraries/openHistorian.XDALink.SqlClr/Properties/AssemblyInfo.cs
+++ Source/Libraries/openHistorian.XDALink.SqlClr/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.4.0.71")]
-[assembly: AssemblyVersion("2.4.0.71")]
-[assembly: AssemblyFileVersion("2.4.0.71")]
+// [assembly: AssemblyVersion("2.4.0.72")]
+[assembly: AssemblyVersion("2.4.0.72")]
+[assembly: AssemblyFileVersion("2.4.0.72")]
