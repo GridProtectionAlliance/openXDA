5a2196b303e47c72c21d34fa25876aa025889c27
gsfbuildbot <support@gridprotectionalliance.org>
2020-01-19 00:06:30 -0500

    openXDA: Version change for build v2.5.0.73-master.

--- Source/Tools/SqlClrAssemblyGen/Properties/AssemblyInfo.cs
+++ Source/Tools/SqlClrAssemblyGen/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.5.0.72")]
-[assembly: AssemblyVersion("2.5.0.72")]
-[assembly: AssemblyFileVersion("2.5.0.72")]
+// [assembly: AssemblyVersion("2.5.0.73")]
+[assembly: AssemblyVersion("2.5.0.73")]
+[assembly: AssemblyFileVersion("2.5.0.73")]
