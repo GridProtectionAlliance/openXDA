3913eed9ad5050777d353d84295eff5631dedc8b
gsfbuildbot <support@gridprotectionalliance.org>
2020-03-14 00:26:33 -0400

    openXDA: Version change for build v2.5.0.109-master.

--- Source/Tools/XDABatchDataTransferTool/Properties/AssemblyInfo.cs
+++ Source/Tools/XDABatchDataTransferTool/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.5.0.108")]
-[assembly: AssemblyVersion("2.5.0.108")]
-[assembly: AssemblyFileVersion("2.5.0.108")]
+// [assembly: AssemblyVersion("2.5.0.109")]
+[assembly: AssemblyVersion("2.5.0.109")]
+[assembly: AssemblyFileVersion("2.5.0.109")]
