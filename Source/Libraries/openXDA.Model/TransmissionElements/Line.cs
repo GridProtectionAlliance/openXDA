2d88ddd314641a17759e5a9bed489173399bf9cf
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-04-15 13:04:09 -0400

    Added Logic to Find Transmission Paths based on Segement connections and sort to ensure longest path is used for FaultLocation

--- Source/Libraries/openXDA.Model/TransmissionElements/Line.cs
+++ Source/Libraries/openXDA.Model/TransmissionElements/Line.cs
@@ -41,6 +41,7 @@ namespace openXDA.Model
         #region [ Members ]
        
         private List<LineSegment> m_lineSegment;
+        private List<TransmissionPath> m_path;
 
         #endregion
 
@@ -70,11 +71,11 @@ namespace openXDA.Model
         {
             get
             {
-                return new List<TransmissionPath>();
+                return m_path ?? (m_path = QueryPath().OrderByDescending(item => item.Length).ToList());
             }
             set
             {
-                //m_lineSegment = value;
+                m_path = value;
             }
         }
 
@@ -125,6 +126,136 @@ namespace openXDA.Model
             return lineSegements;
         }
 
+        private List<TransmissionPath> QueryPath()
+        {
+            
+            //Start by finding all the ends
+            List<LineSegment> lineEnds = Segments.Where(item => item.IsEnd).ToList();
+
+            if (lineEnds.Count == 1)
+                return new List<TransmissionPath>()
+                {
+                    new TransmissionPath()
+                    {
+                        Length = lineEnds[0].Length,
+                        X0 = lineEnds[0].X0,
+                        R0 = lineEnds[0].R0,
+                        X1 = lineEnds[0].X1,
+                        R1 = lineEnds[0].R1,
+                        Line = this,
+                        Segments = new List<LineSegment>() {lineEnds[0]}
+                    }
+                };
+
+            if (lineEnds.Count == 0)
+                return new List<TransmissionPath>()
+                {
+                    new TransmissionPath()
+                    {
+                        Length = 0,
+                        X0 = 0,
+                        R0 = 0,
+                        X1 = 0,
+                        R1 = 0,
+                        Line = this,
+                        Segments = new List<LineSegment>()
+                    }
+                };
+
+
+            Dictionary<Tuple<int, int>, List<LineSegment>> result = new Dictionary<Tuple<int, int>, List<LineSegment>>();
+
+            // start by creating all potential combinations
+
+            for (int i = 1; i < lineEnds.Count; i++)
+            {
+                for (int j = (i-1); j > -1; j--)
+                {
+                    result.Add(new Tuple<int, int>(lineEnds[i].ID, lineEnds[j].ID), new List<LineSegment>()); 
+                }
+            }
+
+            for (int i = 1; i < lineEnds.Count; i++)
+            {
+                FollowPath(lineEnds[i]).ForEach(item =>
+                {
+                    Tuple<int, int> key = new Tuple<int, int>(item[0].ID, item.Last().ID);
+                    if (!result.Keys.Contains(key))
+                    {
+                        key = new Tuple<int, int>(key.Item2, key.Item1);
+                    }
+                    result[key] = item;
+                });
+            }
+
+            result = result.Where(item => item.Value.Count > 0).ToDictionary(item => item.Key, item=> item.Value);
+
+
+            return result.Select( item => new TransmissionPath()
+            {
+                Length = item.Value.Select(seg => seg.Length).Sum(),
+                X0 = item.Value.Select(seg => seg.X0).Sum(),
+                R0 = item.Value.Select(seg => seg.R0).Sum(),
+                X1 = item.Value.Select(seg => seg.X1).Sum(),
+                R1 = item.Value.Select(seg => seg.R1).Sum(),
+                Line = this,
+                Segments = item.Value
+            }).ToList();
+        }
+
+        private List<List<LineSegment>> FollowPath (LineSegment start, LineSegment prev=null)
+        {
+            List<List<LineSegment>> result = new List<List<LineSegment>>();
+
+            List<LineSegment> nextSegments = GetNextSegement(start, prev);
+
+            foreach( LineSegment next in nextSegments)
+            {
+                if (next.IsEnd)
+                { 
+                    result.Add(new List<LineSegment>() { start, next });
+                }
+                else
+                {
+                    foreach (List<LineSegment> path in FollowPath(next, start))
+                        result.Add((new List<LineSegment>() { start }).Concat(path).ToList());
+                }
+            }
+
+            return result;
+        }
+
+        private List<LineSegment> GetNextSegement(LineSegment current, LineSegment previous)
+        {
+            List<LineSegment> result = new List<LineSegment>();
+
+            if (previous == null)
+                return current.connectedSegments.Select(item =>
+                {
+                    if (item.ChildSegment == current.ID)
+                        return item.Parent;
+                    else
+                        return item.Child;
+                }).ToList();
+            
+            foreach (LineSegmentConnections conn in current.connectedSegments)
+            {
+                if (conn.ChildSegment == current.ID && conn.ParentSegment != previous.ID)
+                    result.Add(conn.Parent);
+                if (conn.ParentSegment == current.ID && conn.ChildSegment != previous.ID)
+                    result.Add(conn.Child);
+            }
+
+            List<int> prevConnections = previous.connectedSegments.Select(item =>
+                {
+                    if (item.ChildSegment == previous.ID)
+                        return item.ParentSegment;
+                    else
+                        return item.ChildSegment;
+                }).ToList();
+            
+            return result.Where(item => !prevConnections.Contains(item.ID)).ToList();
+        }
 
         public static Line DetailedLine(Asset asset, AdoDataConnection connection)
         {
