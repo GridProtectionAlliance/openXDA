d0e7bcaa355b7273391d542f154ff20688e6f8ee
StephenCWills <swills@gridprotectionalliance.org>
2020-08-13 14:02:24 -0400

    Modified logic to retrieve PQube trigger from PQDIF file.

--- Source/Libraries/FaultData/DataOperations/PQube/PQubeClassificationOverride.cs
+++ Source/Libraries/FaultData/DataOperations/PQube/PQubeClassificationOverride.cs
@@ -21,11 +21,7 @@
 //
 //******************************************************************************************************
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FaultData.DataAnalysis;
 using FaultData.DataResources;
 using FaultData.DataSets;
