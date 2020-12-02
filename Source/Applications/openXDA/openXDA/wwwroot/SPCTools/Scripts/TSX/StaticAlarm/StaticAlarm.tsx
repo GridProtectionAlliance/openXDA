3ce9a8b3664af5067b2030e4ba1a1d6462a21763
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-12-02 10:24:52 -0500

    Fixed issue with Tooltips on Next button in Wizzard to render Tasks properly

--- Source/Applications/openXDA/openXDA/wwwroot/SPCTools/Scripts/TSX/StaticAlarm/StaticAlarm.tsx
+++ Source/Applications/openXDA/openXDA/wwwroot/SPCTools/Scripts/TSX/StaticAlarm/StaticAlarm.tsx
@@ -30,7 +30,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import StaticSetPoint from './SetPoint';
 import TestGroup from './Test';
 import { SPCTools, StaticWizzard } from '../global';
-import { error } from 'jquery';
+import ReactTooltip from 'react-tooltip';
 
 declare var homePath: string;
 declare var apiHomePath: string;
@@ -49,10 +49,10 @@ const StaticAlarmHome: React.FunctionComponent = (props: {}) => {
 
   
     React.useLayoutEffect(() => {
-        ($('[data-toggle="tooltip"]') as any).tooltip({html: true});
+        //($('[data-toggle="tooltip"]') as any).tooltip({html: true});
 
         return () => {
-            ($('[data-toggle="tooltip"]') as any).tooltip('dispose')
+           //($('[data-toggle="tooltip"]') as any).tooltip('dispose')
         }
     })
 
@@ -87,7 +87,7 @@ const StaticAlarmHome: React.FunctionComponent = (props: {}) => {
                 <div className="col" style={{width: '100%'}}>
                     <div className="btn-group mr-2 float-right" role="group">
                         <button type="button" className={"btn btn-success" + (errors.some(item => (item.complete == 'required')) ? ' disabled' : '')}
-                            onClick={() => { dispatch(next()); }} data-toggle="tooltip" data-placement="top" title={ReactDOMServer.renderToString(<>{errors.map(item => <Requirements {...item} />)}</>)}>
+                            onClick={() => { if (!errors.some(item => (item.complete == 'required'))) dispatch(next()); }} data-tip data-for="registerTip">
                             {loading == 'loading' ? <span className="spinner-border spinner-border-sm"></span> : (tab == "test" ? "Save" : "Continue")}
                         </button> 
                     </div>
@@ -98,6 +98,10 @@ const StaticAlarmHome: React.FunctionComponent = (props: {}) => {
                     </div>
                 </div>
             </div>
+            <ReactTooltip id={'registerTip'} effect="solid" getContent={() => {
+                return (<div style={{ width: '600px' }}> {errors.map((item, index) => <Requirements {...item} key={index} />)} </div>)
+            }}>
+            </ReactTooltip>
         </div>      
     );
 }
@@ -106,19 +110,20 @@ const Requirements = (props: StaticWizzard.IRequirement) => {
     return <div>
         {props.complete == 'complete' ?
             <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check-circle-fill" >
-                <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                <path fillRule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
             </svg> :
             (props.complete == 'required' ?
                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-exclamation-circle-fill">
-                    <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                    <path fillRule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                 </svg>
                 :
                 <svg width="1.0625em" height="1em" viewBox="0 0 17 16" className="bi bi-exclamation-triangle">
-                    <path fill-rule="evenodd" d="M7.938 2.016a.146.146 0 0 0-.054.057L1.027 13.74a.176.176 0 0 0-.002.183c.016.03.037.05.054.06.015.01.034.017.066.017h13.713a.12.12 0 0 0 .066-.017.163.163 0 0 0 .055-.06.176.176 0 0 0-.003-.183L8.12 2.073a.146.146 0 0 0-.054-.057A.13.13 0 0 0 8.002 2a.13.13 0 0 0-.064.016zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
+                    <path fillRule="evenodd" d="M7.938 2.016a.146.146 0 0 0-.054.057L1.027 13.74a.176.176 0 0 0-.002.183c.016.03.037.05.054.06.015.01.034.017.066.017h13.713a.12.12 0 0 0 .066-.017.163.163 0 0 0 .055-.06.176.176 0 0 0-.003-.183L8.12 2.073a.146.146 0 0 0-.054-.057A.13.13 0 0 0 8.002 2a.13.13 0 0 0-.064.016zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
                     <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
                 </svg>)
         }
-        <p>{props.text}</p> </div>
+        <p>{props.text}</p>
+    </div>
 }
 
 export default StaticAlarmHome
