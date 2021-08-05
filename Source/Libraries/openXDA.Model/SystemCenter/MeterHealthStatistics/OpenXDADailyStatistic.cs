af7ed95572a2452d83bc8b7a79c22db62217619e
Billy Ernest <bernest@gridprotectionalliance.org>
2021-08-05 10:23:42 -0400

    Added 30 day return limits to statistics

--- Source/Libraries/openXDA.Model/SystemCenter/MeterHealthStatistics/OpenXDADailyStatistic.cs
+++ Source/Libraries/openXDA.Model/SystemCenter/MeterHealthStatistics/OpenXDADailyStatistic.cs
@@ -26,7 +26,7 @@ using System;
 
 namespace SystemCenter.Model
 {
-    [TableName("OpenXDADailyStatistic"), UseEscapedName, ConfigFileTableNamePrefix]
+    [TableName("OpenXDADailyStatistic"), UseEscapedName, ConfigFileTableNamePrefix, ReturnLimit(30)]
     public class OpenXDADailyStatistic
     {
         [PrimaryKey(true)]
