28499425b1181f6679127d7c2b64019842888663
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-10-14 16:54:43 -0400

    Added Authentication Controller and Node for OpenIDConnect Server

---
+++ Source/Applications/openXDA/openXDA/Controllers/AuthenticationService/AuthenticationController.cs
@@ -0,0 +1,266 @@
+﻿//******************************************************************************************************
+//  Config/QueryController.cs - Gbtc
+//
+//  Copyright © 2018, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/25/2018 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using GSF.Data;
+using GSF.Security;
+using GSF.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using openXDA.Model;
+using openXDA.Nodes;
+using openXDA.Nodes.Types.Authentication;
+
+namespace openXDA.Controllers
+{
+    [RoutePrefix("api/auth")]
+    public class AuthenticationController : ApiController
+    {
+        private Host Host { get; }
+
+        public AuthenticationController(Host host) =>
+            Host = host;
+
+
+        [Route("logon"), HttpGet]
+        public IHttpActionResult BaseLoginRequest(CancellationToken token)
+        {
+            try
+            {
+                // Redirect to the Correct URI
+                Dictionary<string, string> query = Request.QueryParameters();
+
+                string redirectUri;
+                string nonce;
+                string responseType;
+                string scope;
+                string clientId;
+
+                if (!query.TryGetValue("redirect_uri", out redirectUri))
+                    throw new Exception("GET Parameter 'redirect_uri' is required");
+                if (!query.TryGetValue("nonce", out nonce))
+                    throw new Exception("GET Parameter 'nonce' is required");
+                if (!query.TryGetValue("response_type", out responseType))
+                    throw new Exception("GET Parameter 'responseType' is required");
+                if (!query.TryGetValue("scope", out scope))
+                    throw new Exception("GET Parameter 'scope' is required");
+                if (!query.TryGetValue("client_id", out clientId))
+                    throw new Exception("GET Parameter 'clientId' is required");
+
+                if (responseType != "code")
+                    throw new Exception("Only ResponseType code is supported");
+
+                UserData user = ((SecurityPrincipal)Request.GetRequestContext().Principal).Identity.Provider.UserData;
+
+                const string QueryFormat =
+                "SELECT Node.ID " +
+                "FROM " +
+                "    ActiveHost JOIN " +
+                "    Node ON Node.HostRegistrationID = ActiveHost.ID JOIN " +
+                "    NodeType ON Node.NodeTypeID = NodeType.ID " +
+                "WHERE NodeType.TypeName = {0}";
+
+                Type nodeType = typeof(AuthenticationProviderNode);
+                string typeName = nodeType.FullName;
+                int? result;
+
+                using (AdoDataConnection connection = new AdoDataConnection("systemSettings"))
+                    result = connection.ExecuteScalar<int?>(QueryFormat, typeName);
+
+                if (result is null)
+                    throw new Exception($"No node of type '{typeName}' was found.");
+
+
+                void ConfigureRequest(HttpRequestMessage request)
+                {
+                    int nodeID = result.GetValueOrDefault();
+                    string action = "AuthorizeCode";
+                    NameValueCollection queryParameters = new NameValueCollection();
+                    queryParameters.Add("appId", clientId ?? "") ;
+                    queryParameters.Add("userId", user.LoginID ?? "");
+                    queryParameters.Add("userName", user.Username ?? "");
+                    queryParameters.Add("userGivenName", user.FirstName ?? "");
+                    queryParameters.Add("userLastName", user.LastName ?? "");
+                    queryParameters.Add("userPhone", user.PhoneNumber ?? "");
+                    queryParameters.Add("userEmail", user.EmailAddress ?? "");
+                    queryParameters.Add("userRoles", string.Join(",", user.Roles) ?? "");
+                             
+
+                    string url = Host.BuildURL(nodeID, action, queryParameters);
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(url);
+                }
+
+                string code;
+                using (HttpResponseMessage response = Host.SendWebRequestAsync(ConfigureRequest).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                        code = response.Content.ReadAsStringAsync().Result.Trim('"');
+                    else
+                        throw new Exception($"Unable to get code from Authentication Node: {response.Content.ReadAsStringAsync().Result}");
+                }
+
+                return Redirect($"{redirectUri}?code={code}");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("token"), HttpPost]
+        public IHttpActionResult RequestUserInfo([FromBody] JObject data)
+        {
+            try
+            {
+                // Redirect to the Correct URI
+                JToken grantType;
+                JToken clientId;
+                JToken code;
+                JToken redirectURI;
+
+                if (!data.TryGetValue("grant_type", out grantType))
+                    throw new Exception("POST Parameter 'grant_type' is required");
+                if (!data.TryGetValue("client_id", out clientId))
+                    throw new Exception("POST Parameter 'client_id' is required");
+                if (!data.TryGetValue("code", out code))
+                    throw new Exception("POST Parameter 'code' is required");
+                if (!data.TryGetValue("redirect_uri", out redirectURI))
+                    throw new Exception("POST Parameter 'redirect_uri' is required");
+
+                if (grantType.ToString().ToLower() != "authorization_code")
+                    throw new Exception("Only grant type authorization_code is accepted");
+
+               const string QueryFormat =
+               "SELECT Node.ID " +
+               "FROM " +
+               "    ActiveHost JOIN " +
+               "    Node ON Node.HostRegistrationID = ActiveHost.ID JOIN " +
+               "    NodeType ON Node.NodeTypeID = NodeType.ID " +
+               "WHERE NodeType.TypeName = {0}";
+
+                Type nodeType = typeof(AuthenticationProviderNode);
+                string typeName = nodeType.FullName;
+                int? result;
+
+                using (AdoDataConnection connection = new AdoDataConnection("systemSettings"))
+                    result = connection.ExecuteScalar<int?>(QueryFormat, typeName);
+
+                if (result is null)
+                    throw new Exception($"No node of type '{typeName}' was found.");
+
+                void ConfigureRequest(HttpRequestMessage request)
+                {
+                    int nodeID = result.GetValueOrDefault();
+                    string action = "GetToken";
+                    NameValueCollection queryParameters = new NameValueCollection();
+                    queryParameters.Add("appId", clientId.ToString() ?? "");
+                    queryParameters.Add("code", code.ToString());
+
+
+                    string url = Host.BuildURL(nodeID, action, queryParameters);
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(url);
+                }
+
+                using (HttpResponseMessage response = Host.SendWebRequestAsync(ConfigureRequest).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        JObject token = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                        
+                        if (!token.TryGetValue("access_token", out JToken accessToken) || string.IsNullOrEmpty(accessToken.ToString()))
+                            throw new Exception("Token not available for this code and clientId");
+
+                        return Ok(token);
+                    }
+                        
+                    else
+                        throw new Exception($"Unable to get code from Authentication Node: {response.Content.ReadAsStringAsync().Result}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+
+
+        private async Task<HttpResponseMessage> SendNodeRequestAsync(Action<HttpRequestMessage> configure, CancellationToken cancellationToken = default)
+        {
+            const string HostQueryFormat =
+                "SELECT " +
+                "    HostRegistration.RegistrationKey, " +
+                "    HostRegistration.APIToken " +
+                "FROM HostRegistration LEFT JOIN Node ON HostRegistration.ID = Node.HostRegistrationID " +
+                "WHERE Node.Name='AutheticationProvider'";
+
+            string registrationKey;
+            string apiToken;
+
+            using (AdoDataConnection connection = new AdoDataConnection("systemSettings"))
+            using (DataTable hostResult = connection.RetrieveData(HostQueryFormat))
+            {
+                if (hostResult.Rows.Count == 0)
+                    throw new InvalidOperationException($"AuthorizationProvider is not active.");
+
+                DataRow hostRow = hostResult.Rows[0];
+                registrationKey = hostRow.ConvertField<string>("RegistrationKey");
+                apiToken = hostRow.ConvertField<string>("APIToken");
+            }
+
+            using (HttpRequestMessage request = new HttpRequestMessage())
+            {
+                configure(request);
+
+                const string type = "XDA-Host";
+                string decode = $"{registrationKey}:{apiToken}";
+                Encoding utf8 = new UTF8Encoding(false);
+                byte[] credentialData = utf8.GetBytes(decode);
+                string credentials = Convert.ToBase64String(credentialData);
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(type, credentials);
+
+                return await HttpClient.SendAsync(request, cancellationToken);
+            }
+        }
+
+        #region [ Static ]
+        // Static Properties
+        private static HttpClient HttpClient { get; }
+            = new HttpClient();
+
+        #endregion
+    }
+}
---
+++ Source/Libraries/openXDA.Nodes/Types/Authentication/AuthenticationProviderNode.cs
@@ -0,0 +1,198 @@
+﻿//******************************************************************************************************
+//  AuthenticationProviderNode.cs - Gbtc
+//
+//  Copyright © 2021, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  10/07/2021 - C. Lackner
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using FaultData.DataAnalysis;
+using FaultData.DataOperations;
+using GSF.Collections;
+using GSF.Configuration;
+using GSF.Data;
+using GSF.Data.Model;
+using GSF.Security;
+using log4net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using openXDA.Configuration;
+using openXDA.Model;
+
+namespace openXDA.Nodes.Types.Authentication
+{
+    public class AuthenticationProviderNode : NodeBase
+    {
+        #region [ Members ]
+
+        private class Settings
+        {
+            public Settings(Action<object> configure) =>
+                configure(this);
+        }
+
+        private class UserInfoAuth
+        {
+            public string UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public string Roles { get; set; }
+            public DateTime Expires { get; set; }
+            public string AppId { get; set; }
+        }
+
+        public class TokenResponse
+        {
+            public string access_token { get; set; }
+            public string id_token { get; set; }
+        }
+
+        private class AutheticationProviderWebController : ApiController
+        {
+            private AuthenticationProviderNode Node { get; }
+
+            public AutheticationProviderWebController(AuthenticationProviderNode node) =>
+                Node = node;
+
+            [HttpPost]
+            public IHttpActionResult AuthorizeCode(string userId, string userName, string userGivenName, string userLastName, string userPhone, string userEmail, string userRoles, string appId) =>
+                Ok(Node.GenerateCode(userId, userName, userGivenName, userLastName, userPhone, userEmail, userRoles, appId));
+
+            [HttpPost]
+            public IHttpActionResult GetToken(string code, string appId) => Ok(Node.GenerateToken(code, appId));
+        }
+
+        #endregion
+
+        #region [ Constructors ]
+
+        public AuthenticationProviderNode(Host host, Node definition, NodeType type)
+            : base(host, definition, type)
+        {
+            Configurator = GetConfigurator();
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        private Action<object> Configurator { get; set; }
+
+        #endregion
+
+        #region [ Methods ]
+
+
+        public string GenerateCode(string userId, string userName, string userGivenName, string userLastName, string userPhone, string userEmail, string userRoles, string appId)
+        {
+          
+            byte[] codeBytes = new byte[16];
+            new Random().NextBytes(codeBytes);
+
+            string code = BitConverter.ToString(codeBytes).Replace("-", "");
+            s_codeStore.Add(code, new UserInfoAuth()
+            {
+                UserId = userId,
+                UserName = userName,
+                FirstName = userGivenName,
+                LastName = userLastName,
+                Email = userEmail,
+                Phone = userPhone,
+                Roles = userRoles,
+                Expires = DateTime.UtcNow.AddMinutes(10),
+                AppId = appId
+            });
+
+            s_codeStore = s_codeStore.Where(item => item.Value.Expires > DateTime.UtcNow).ToDictionary(i => i.Key, i => i.Value);
+
+            return code;
+        }
+
+        public TokenResponse GenerateToken(string code, string appId)
+        {
+            UserInfoAuth user;
+            if (!s_codeStore.TryGetValue(code, out user))
+                return new TokenResponse() { id_token = "", access_token = "" };
+
+            if (user.AppId != appId)
+                return new TokenResponse() { id_token = "", access_token = "" };
+
+            if (user.Expires < DateTime.UtcNow)
+                return new TokenResponse() { id_token = "", access_token = "" };
+
+            JObject header = new JObject();
+            header.Add("alg", "None");
+            header.Add("typ", "JWT");
+            header.Add("kid", "None");
+
+            JObject idToken = new JObject();
+            idToken.Add("sub", user.UserId);
+            idToken.Add("name", user.UserName);
+            idToken.Add("given_name", user.FirstName);
+            idToken.Add("family_name", user.LastName);
+            idToken.Add("phone_number", user.Phone);
+            idToken.Add("email", user.Email);
+            idToken.Add("roles", user.Roles);
+            string token = Convert.ToBase64String(Encoding.UTF8.GetBytes(header.ToString())) + "." + Convert.ToBase64String(Encoding.UTF8.GetBytes(idToken.ToString()));
+
+            
+            return new TokenResponse() { id_token= token, access_token = token };
+        }
+
+        protected override void OnReconfigure(Action<object> configurator) =>
+            Configurator = configurator;
+
+        public override IHttpController CreateWebController() =>
+            new AutheticationProviderWebController(this);
+
+      
+
+        
+        private void Configure(object obj) => Configurator(obj);
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Fields
+        private static readonly ConnectionStringParser<SettingAttribute, CategoryAttribute> ConnectionStringParser = new ConnectionStringParser<SettingAttribute, CategoryAttribute>();
+        private static readonly ILog Log = LogManager.GetLogger(typeof(AuthenticationProviderNode));
+        private static Dictionary<string, UserInfoAuth> s_codeStore = new Dictionary<string, UserInfoAuth>();
+
+      
+        #endregion
+    }
+}
