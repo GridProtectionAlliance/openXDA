9049aaed5ed4dfac46732655b1a0b1e0d5c92935
gsfbuildbot <support@gridprotectionalliance.org>
2021-10-21 00:07:27 -0400

    openXDA: Version change for build v3.0.3.133-master.

--- Source/Libraries/SPCTools/Properties/AssemblyInfo.cs
+++ Source/Libraries/SPCTools/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.132")]
-[assembly: AssemblyVersion("3.0.3.132")]
-[assembly: AssemblyFileVersion("3.0.3.132")]
+// [assembly: AssemblyVersion("3.0.3.133")]
+[assembly: AssemblyVersion("3.0.3.133")]
+[assembly: AssemblyFileVersion("3.0.3.133")]
