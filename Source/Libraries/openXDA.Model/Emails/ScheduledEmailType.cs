0e77f210ff0bad87d73c53fb50a9769b86316d58
Samuel Robinson <srobinson@gridprotectionalliance.org>
2021-11-19 12:02:29 -0500

    made public

--- Source/Libraries/openXDA.Model/Emails/ScheduledEmailType.cs
+++ Source/Libraries/openXDA.Model/Emails/ScheduledEmailType.cs
@@ -25,7 +25,7 @@ using GSF.Data.Model;
 
 namespace openXDA.Model.Emails
 {
-    class ScheduledEmailType
+    public class ScheduledEmailType
     {
         [PrimaryKey(true)]
         public int ID { get; set; }
