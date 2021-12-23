dbae1cde50cf29b1aa7da82a1f06eabfa0b63420
gsfbuildbot <support@gridprotectionalliance.org>
2021-12-23 00:29:54 -0500

    openXDA: Version change for build v3.0.3.181-master.

--- Source/Libraries/openXDA.XMLConfigLoader/Properties/AssemblyInfo.cs
+++ Source/Libraries/openXDA.XMLConfigLoader/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.180")]
-[assembly: AssemblyVersion("3.0.3.180")]
-[assembly: AssemblyFileVersion("3.0.3.180")]
+// [assembly: AssemblyVersion("3.0.3.181")]
+[assembly: AssemblyVersion("3.0.3.181")]
+[assembly: AssemblyFileVersion("3.0.3.181")]
 [assembly: NeutralResourcesLanguage("")]
--- Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
+++ Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.180")]
-[assembly: AssemblyVersion("3.0.3.180")]
-[assembly: AssemblyFileVersion("3.0.3.180")]
+// [assembly: AssemblyVersion("3.0.3.181")]
+[assembly: AssemblyVersion("3.0.3.181")]
+[assembly: AssemblyFileVersion("3.0.3.181")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
