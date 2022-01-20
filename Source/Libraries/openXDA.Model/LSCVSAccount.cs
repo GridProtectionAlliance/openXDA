84270185c6d9babf7d2645ecebc016e15ccc99fc
Samuel Robinson <srobinson@gridprotectionalliance.org>
2022-01-20 15:56:00 -0500

    Creaated model for LSCVSAccount

---
+++ Source/Libraries/openXDA.Model/LSCVSAccount.cs
@@ -0,0 +1,19 @@
+﻿using GSF.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCenter.Model
+{
+    public class LSCVSAccount
+    {
+        [PrimaryKey(true)]
+        public int ID { get; set; }
+
+        public string AccountID { get; set; }
+
+        public int CustomerID { get; set; }
+    }
+}
