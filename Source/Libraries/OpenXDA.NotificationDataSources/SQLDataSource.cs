eed1d179332db250cabaad73bb6ee6227cdbc9fc
StephenCWills <swills@gridprotectionalliance.org>
2022-03-11 18:10:12 -0500

    Add header comments to SQLDataSource

--- Source/Libraries/OpenXDA.NotificationDataSources/SQLDataSource.cs
+++ Source/Libraries/OpenXDA.NotificationDataSources/SQLDataSource.cs
@@ -1,12 +1,31 @@
-﻿using GSF.Configuration;
+﻿//******************************************************************************************************
+//  SQLDataSource.cs - Gbtc
+//
+//  Copyright © 2022, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/13/2022 - Christoph Lackner
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Xml.Linq;
+using GSF.Configuration;
 using GSF.Data;
 using openXDA.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace openXDA.NotificationDataSources
 {
@@ -33,10 +52,10 @@ namespace openXDA.NotificationDataSources
             Definition = definition;
             Connectionfactory = connectionFactory;
         }
+
         #endregion
 
-        #region [ Properties]
-
+        #region [ Properties ]
       
         public TriggeredEmailDataSource Definition { get; }
 
@@ -45,7 +64,6 @@ namespace openXDA.NotificationDataSources
         #endregion
 
         #region [ Methods ]
-
         
         protected Action<object> GetConfigurator()
         {
