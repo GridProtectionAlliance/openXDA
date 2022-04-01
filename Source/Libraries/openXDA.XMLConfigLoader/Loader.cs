a13ccf486316e9780974eb66195ecf1c6d5f96e7
StephenCWills <swills@gridprotectionalliance.org>
2022-04-01 15:49:15 -0400

    Remove and sort usings in DeviceDefinitionsMigrator

--- Source/Libraries/openXDA.XMLConfigLoader/Loader.cs
+++ Source/Libraries/openXDA.XMLConfigLoader/Loader.cs
@@ -24,8 +24,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using GSF.Data;
--- Source/Tools/DeviceDefinitionsMigrator/Program.cs
+++ Source/Tools/DeviceDefinitionsMigrator/Program.cs
@@ -27,9 +27,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Xml.Linq;
 using GSF.Collections;
