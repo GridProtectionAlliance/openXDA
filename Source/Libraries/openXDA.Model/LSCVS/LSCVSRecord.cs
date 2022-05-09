e96601797b6948b27801bc37dabc767d7b08801e
gcsantos <gcsantos@gridprotectionalliance.org>
2022-05-09 12:42:29 -0400

    Fixed Meter/Customer/EventType Field Typings and names to match what is expected by LSCVSReport v2.0

--- Source/Libraries/openXDA.Model/LSCVS/LSCVSRecord.cs
+++ Source/Libraries/openXDA.Model/LSCVS/LSCVSRecord.cs
@@ -43,10 +43,9 @@ namespace LSCVS.Model
         public DateTime EventStart { get; set; }
         public double Duration { get; set; }
         public double Magnitude { get; set; }
-        public int MeterID { get; set; }
-        public int CustomerID { get; set; }
-        public int EventTypeID { get; set; }
-        public bool InsideCurve { get; set; }
+        public string Meter { get; set; }
+        public string Customer { get; set; }
+        public string EventCurveCombined { get; set; }
         public double IntialMW { get; set; }
         public double FinalMW { get; set; }
         public bool Reviewed { get; set; }
