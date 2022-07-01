8e6c42fdb06f7a7498c3ca276b53c15292080632
Christoph Lackner <clackner@gridprotectionalliance.org>
2022-07-01 13:00:35 -0400

    Added API Authentication Library

---
+++ Source/Libraries/openXDA.APIAuthentication/APIAccessKey.cs
@@ -0,0 +1,48 @@
+﻿//******************************************************************************************************
+//  APIAccessKey.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  07/01/2022 - Christoph Lackner
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+
+using GSF.Data.Model;
+using System;
+
+namespace openXDA.APIAuthentication
+{
+    /// <summary>
+    /// Model that represents a valid set of credentials to access the API.
+    /// </summary>
+    public class APIAccessKey
+    {
+        #region[Properties]
+        public string APIToken { get; set; }
+        public string RegistrationKey { get; set; }
+
+        [PrimaryKey(true)]
+        public int ID { get; set; }
+
+        public DateTime Expires { get; set; }
+        #endregion[Properties]
+
+    }
+
+
+}
