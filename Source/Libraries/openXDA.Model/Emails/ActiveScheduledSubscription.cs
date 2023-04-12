fe73f4b45e047e4052b089ca046007baeb72b499
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2023-04-12 15:33:18 -0400

    Added ActiveScheduledEmailSubsciption model

---
+++ Source/Libraries/openXDA.Model/Emails/ActiveScheduledSubscription.cs
@@ -0,0 +1,44 @@
+﻿//******************************************************************************************************
+//  AciveScheduledSubscription.cs - Gbtc
+//
+//  Copyright © 2022, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  04/12/2023 - C Lackner
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using GSF.Data.Model;
+using GSF.Identity;
+
+namespace openXDA.Model
+{
+	[AllowSearch]
+	public class ActiveScheduledSubscription
+	{
+		public int UserAccountScheduledEmailID { get; set; }
+
+		[ParentKey(typeof(ConfirmableUserAccount))]
+		public Guid UserAccountID { get; set; }
+		public string AssetGroup { get; set; }
+		public string EmailName { get; set; }
+		public string Category { get; set; }
+		public int ScheduledEmailTypeID { get; set; }
+		public string Email { get; set; }
+		public string UserName { get; set; }
+	}
+}
