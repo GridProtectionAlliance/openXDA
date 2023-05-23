5fc0af1703cf5df6a07786276fba27dc0a4fc497
gsfbuildbot <support@gridprotectionalliance.org>
2023-05-23 01:44:18 -0400

    openXDA: Version change for build v3.0.3.372-master.

--- Source/Libraries/openXDA.XMLConfigLoader/Properties/AssemblyInfo.cs
+++ Source/Libraries/openXDA.XMLConfigLoader/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.371")]
-[assembly: AssemblyVersion("3.0.3.371")]
-[assembly: AssemblyFileVersion("3.0.3.371")]
+// [assembly: AssemblyVersion("3.0.3.372")]
+[assembly: AssemblyVersion("3.0.3.372")]
+[assembly: AssemblyFileVersion("3.0.3.372")]
 [assembly: NeutralResourcesLanguage("")]
--- Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
+++ Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.371")]
-[assembly: AssemblyVersion("3.0.3.371")]
-[assembly: AssemblyFileVersion("3.0.3.371")]
+// [assembly: AssemblyVersion("3.0.3.372")]
+[assembly: AssemblyVersion("3.0.3.372")]
+[assembly: AssemblyFileVersion("3.0.3.372")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
