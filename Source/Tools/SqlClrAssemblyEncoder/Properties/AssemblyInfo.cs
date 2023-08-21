5c9967a99aa9f5b6e7dadefcc8d89452cd78ec87
gsfbuildbot <support@gridprotectionalliance.org>
2023-08-21 15:24:04 -0400

    openXDA: Version change for build v3.0.3.393-master.

--- Source/Tools/SqlClrAssemblyEncoder/Properties/AssemblyInfo.cs
+++ Source/Tools/SqlClrAssemblyEncoder/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.392")]
-[assembly: AssemblyVersion("3.0.3.392")]
-[assembly: AssemblyFileVersion("3.0.3.392")]
+// [assembly: AssemblyVersion("3.0.3.393")]
+[assembly: AssemblyVersion("3.0.3.393")]
+[assembly: AssemblyFileVersion("3.0.3.393")]
