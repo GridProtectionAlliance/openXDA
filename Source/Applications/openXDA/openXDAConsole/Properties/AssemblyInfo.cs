6710d3e9e1c5c492a32b4b4fa8067d8ab23e02ce
gsfbuildbot <support@gridprotectionalliance.org>
2023-10-18 00:27:58 -0400

    openXDA: Version change for build v3.0.3.406-master.

--- Source/Applications/openXDA/openXDAConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openXDA/openXDAConsole/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.405")]
-[assembly: AssemblyVersion("3.0.3.405")]
-[assembly: AssemblyFileVersion("3.0.3.405")]
+// [assembly: AssemblyVersion("3.0.3.406")]
+[assembly: AssemblyVersion("3.0.3.406")]
+[assembly: AssemblyFileVersion("3.0.3.406")]
