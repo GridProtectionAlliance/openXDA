db70a215088a032f39d02ff12abf8e37993a56ba
gsfbuildbot <support@gridprotectionalliance.org>
2024-02-24 00:26:37 -0500

    openXDA: Version change for build v3.0.3.436-master.

--- Source/Libraries/openXDA.ArcGIS/Properties/AssemblyInfo.cs
+++ Source/Libraries/openXDA.ArcGIS/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.435")]
-[assembly: AssemblyVersion("3.0.3.435")]
-[assembly: AssemblyFileVersion("3.0.3.435")]
+// [assembly: AssemblyVersion("3.0.3.436")]
+[assembly: AssemblyVersion("3.0.3.436")]
+[assembly: AssemblyFileVersion("3.0.3.436")]
 
--- Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
+++ Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.435")]
-[assembly: AssemblyVersion("3.0.3.435")]
-[assembly: AssemblyFileVersion("3.0.3.435")]
+// [assembly: AssemblyVersion("3.0.3.436")]
+[assembly: AssemblyVersion("3.0.3.436")]
+[assembly: AssemblyFileVersion("3.0.3.436")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
