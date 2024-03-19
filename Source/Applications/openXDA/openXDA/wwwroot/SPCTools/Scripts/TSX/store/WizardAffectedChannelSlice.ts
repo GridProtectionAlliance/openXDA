cdaa61dbfe24c349b7ac2a14fe745d66b7062dce
prestoncraw <prestoncrawford530@gmail.com>
2024-03-19 08:44:20 -0400

    Fix issues in searchFilters for calls to SearchableList endpoint

--- Source/Applications/openXDA/openXDA/wwwroot/SPCTools/Scripts/TSX/store/WizardAffectedChannelSlice.ts
+++ Source/Applications/openXDA/openXDA/wwwroot/SPCTools/Scripts/TSX/store/WizardAffectedChannelSlice.ts
@@ -105,7 +105,7 @@ function GetAffectedChannels(meterIDs: number[], measurementTypeID: number, seri
     let filter = [
         {
             FieldName: "MeterID",
-            SearchText: `(${(meterIDs.length > 0 ? meterIDs.join(',') : 0)})`,
+            SearchText: `${(meterIDs.length > 0 ? meterIDs.join(',') : 0)}`,
             Operator: "IN",
             Type: "integer"
         },
@@ -113,13 +113,13 @@ function GetAffectedChannels(meterIDs: number[], measurementTypeID: number, seri
             FieldName: "MeasurementTypeID",
             SearchText: `(SELECT ChannelGroupType.MeasurementTypeID FROM ChannelGroupType WHERE ID = ${measurementTypeID})`,
             Operator: "=",
-            Type: "integer"
+            Type: "query"
         },
         {
             FieldName: "MeasurementCharacteristicID",
             SearchText: `(SELECT ChannelGroupType.MeasurementCharacteristicID FROM ChannelGroupType WHERE ID = ${measurementTypeID})`,
             Operator: "=",
-            Type: "integer"
+            Type: "query"
         },
         {
             FieldName: `(SELECT COUNT(Series.ID) FROM SERIES WHERE Series.ChannelID = FullTbl.ID AND Series.SeriesTypeID = ${seriesTypeID})`,
@@ -129,13 +129,13 @@ function GetAffectedChannels(meterIDs: number[], measurementTypeID: number, seri
         },
         {
             FieldName: "PhaseID",
-            SearchText: `(${(phaseIDs.length > 0 ? phaseIDs.join(',') : 0)})`,
+            SearchText: `${(phaseIDs.length > 0 ? phaseIDs.join(',') : 0)}`,
             Operator: "IN",
             Type: "integer"
         },
         {
             FieldName: "(SELECT VoltageKV FROM Asset WHERE Asset.ID = FullTbl.AssetID)",
-            SearchText: `(${(voltages.length > 0 ? voltages.join(',') : 0)})`,
+            SearchText: `${(voltages.length > 0 ? voltages.join(',') : 0)}`,
             Operator: "IN",
             Type: "query"
         }
