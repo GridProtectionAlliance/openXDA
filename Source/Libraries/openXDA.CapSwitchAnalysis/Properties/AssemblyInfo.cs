0c7fbad7f9ecdc0535ced47c86aebc65515bb178
gsfbuildbot <support@gridprotectionalliance.org>
2024-05-19 02:10:24 -0400

    openXDA: Version change for build v3.0.3.460-master.

--- Source/Libraries/openXDA.CapSwitchAnalysis/Properties/AssemblyInfo.cs
+++ Source/Libraries/openXDA.CapSwitchAnalysis/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.3.459")]
-[assembly: AssemblyVersion("3.0.3.459")]
-[assembly: AssemblyFileVersion("3.0.3.459")]
+// [assembly: AssemblyVersion("3.0.3.460")]
+[assembly: AssemblyVersion("3.0.3.460")]
+[assembly: AssemblyFileVersion("3.0.3.460")]
