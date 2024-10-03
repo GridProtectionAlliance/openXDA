f5783e3707357b1810e12fabe659fd9f58ea8e85
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2024-10-03 09:28:40 -0400

    Removed legacy authenthication from datapusher

--- Source/Libraries/FaultData/DataOperations/DataPusherOperation.cs
+++ Source/Libraries/FaultData/DataOperations/DataPusherOperation.cs
@@ -114,7 +114,7 @@ namespace FaultData.DataOperations
                     post.MeterKey = metersToDataPush.RemoteXDAAssetKey;
                     Log.Info($"Sending data to instance: {instance.Name} for FileGroup: {meterDataSet.FileGroup.ID}...");
                     DataPusherEngine engine = new DataPusherEngine(meterDataSet.CreateDbConnection);
-                    DataPusherRequester requester = new DataPusherRequester(instance, connection);
+                    DataPusherRequester requester = new DataPusherRequester(instance);
                     engine.SendFiles(post, requester);
 
 
