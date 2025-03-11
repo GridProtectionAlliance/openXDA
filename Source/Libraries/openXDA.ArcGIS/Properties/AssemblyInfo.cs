86e8315f54072fb3b1ac28ff72b4597705ed3cfb
gsfbuildbot <support@gridprotectionalliance.org>
2025-03-11 02:10:31 -0400

    openXDA: Version change for build v3.0.4.81-master.

--- Source/Libraries/openXDA.ArcGIS/Properties/AssemblyInfo.cs
+++ Source/Libraries/openXDA.ArcGIS/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.4.80")]
-[assembly: AssemblyVersion("3.0.4.80")]
-[assembly: AssemblyFileVersion("3.0.4.80")]
+// [assembly: AssemblyVersion("3.0.4.81")]
+[assembly: AssemblyVersion("3.0.4.81")]
+[assembly: AssemblyFileVersion("3.0.4.81")]
 
