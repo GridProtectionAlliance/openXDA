1ebd0bae432a6b131e817690df225300a2ec0e8e
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2025-10-21 11:08:30 -0400

    rework helper to be static

---
+++ Source/Libraries/openXDA.APIAuthentication/IAPICredentialRetriever.cs
@@ -0,0 +1,58 @@
+﻿//******************************************************************************************************
+//  XDAAPIHelper.cs - Gbtc
+//
+//  Copyright © 2023, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  10/17/2025 - Gabriel Santos
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+namespace openXDA.APIAuthentication
+{
+    /// <summary>
+    /// Interface of credential retriever object for XDA API helper.
+    /// </summary>
+    public interface IAPICredentialRetriever
+    {
+        #region [ Properties ]
+
+        /// <summary>
+        /// API Token used to access OpenXDA
+        /// </summary>
+        string Token { get; }
+
+        /// <summary>
+        /// API Key used to access OpenXDA
+        /// </summary>
+        string Key { get; }
+
+        /// <summary>
+        /// API Key used to access OpenXDA
+        /// </summary>
+        string Host { get; }
+
+        /// <summary>
+        /// Function for fetching or refreshing settings. 
+        /// </summary>
+        /// <remarks>
+        /// This should refresh and update <see cref="Token"/>, <see cref="Key"/>, and <see cref="Host"/> settings.
+        /// </remarks>
+        bool TryRefreshSettings();
+
+        #endregion
+    }
+}
