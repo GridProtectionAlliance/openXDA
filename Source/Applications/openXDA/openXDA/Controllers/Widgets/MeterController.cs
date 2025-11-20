99ec9d8dcf14f9babfffdccf15992704a470f6d7
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2025-11-20 15:11:10 -0500

    added meter controller to support PQHealthIndex widget

---
+++ Source/Applications/openXDA/openXDA/Controllers/Widgets/MeterController.cs
@@ -0,0 +1,35 @@
+//******************************************************************************************************
+//  EventWidgetViewController.cs - Gbtc
+//
+//  Copyright � 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  10/28/2025 - G. Santos
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System.Web.Http;
+using GSF.Web.Model;
+using SystemCenter.Model;
+
+namespace openXDA.Controllers.Widgets
+{
+    /// <summary>
+    /// Controller that handles fetching and searching of the <see cref="DetailedMeter"/> model.
+    /// </summary>
+    [RoutePrefix("api/Widgets/DetailedMeter")]
+    public class DetailedWidgetMeterController : ModelController<DetailedMeter> { }
+}
