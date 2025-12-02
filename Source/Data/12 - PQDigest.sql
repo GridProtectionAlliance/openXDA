2d2c27eb15b0ed714f10199852f1c3a6e4d4db61
Christoph Lackner <clackner@gridprotectionalliance.org>
2025-12-02 07:34:13 -0500

    Added Model for PQDigest Widgets

--- Source/Data/12 - PQDigest.sql
+++ Source/Data/12 - PQDigest.sql
@@ -18,3 +18,45 @@ INSERT INTO [PQDigest.Setting](Name, Value, DefaultValue) VALUES('XDA.APIKey', '
 GO
 INSERT INTO [PQDigest.Setting](Name, Value, DefaultValue) VALUES('XDA.APIToken', '', '')
 GO
+
+CREATE TABLE [PQBrowser.EventViewWidget] (
+    ID INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+    Name varchar(50) NOT NULL DEFAULT(1),
+    Setting varchar(max) NOT NULL Default(''),
+    Type varchar(max) NOT NULL Default('OpenSEE')
+)
+GO
+
+INSERT [PQBrowser.EventViewWidget] (Name, Type) VALUES ('OpenSEE','OpenSEE')
+GO
+
+INSERT [PQBrowser.EventViewWidget] (Name, Type) VALUES ('TrendGraph','TrendGraph')
+GO
+
+INSERT [PQBrowser.EventViewWidget] (Name, Type) VALUES ('PQICurves','PQICurves')
+GO
+
+INSERT [PQBrowser.EventViewWidget] (Name, Type) VALUES ('pqi','pqi')
+GO
+
+
+CREATE TABLE [PQBrowser.HomeScreenWidget] (
+    ID INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+    Name varchar(50) NOT NULL DEFAULT(1),
+    Setting varchar(max) NOT NULL Default(''),
+    Type varchar(max) NOT NULL Default('OpenSEE'),
+    TimeFrame INT NOT NULL Default(30)
+)
+
+INSERT [PQBrowser.HomeScreenWidget] (Name, Type, TimeFrame) VALUES ('EPRI PQ Health Index - Last 30 Days','PQHealthIndex', 30)
+GO
+
+INSERT [PQBrowser.HomeScreenWidget] (Name, Type, TimeFrame) VALUES ('Historical Event Counts - Last 30 Days','EventCountChart', 30)
+GO
+
+INSERT [PQBrowser.HomeScreenWidget] (Name, Type, TimeFrame) VALUES ('Magnitude Duration Chart - Last 30 Days','MagDurChart', 30)
+GO
+
+INSERT [PQBrowser.HomeScreenWidget] (Name, Type, TimeFrame) VALUES ('Meter Activity - Last 30 Days','EventCountTable', 30)
+GO
+
