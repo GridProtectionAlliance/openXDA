7e606805ce7a4ecc7095d45161b42af423be2a05
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-06-02 10:04:52 -0400

    clarify condition

--- Source/Applications/openXDA/openXDA/Controllers/SystemCenter/SystemCenterController.cs
+++ Source/Applications/openXDA/openXDA/Controllers/SystemCenter/SystemCenterController.cs
@@ -254,7 +254,9 @@ namespace openXDA.Controllers.Config
             string connectionString = settings.LightningDataSettings.MaximoConnectionString;
             string dataProviderString = settings.LightningDataSettings.MaximoDataProviderString;
 
-            if (!(String.IsNullOrWhiteSpace(connectionString) && String.IsNullOrWhiteSpace(dataProviderString)))
+            bool missingString = String.IsNullOrWhiteSpace(connectionString) || String.IsNullOrWhiteSpace(dataProviderString);
+
+            if (!missingString)
             {
                 try
                 {
