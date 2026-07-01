d327be70316dc73bbc4034a2a726c72473d1468f
gsfbuildbot <support@gridprotectionalliance.org>
2026-07-01 15:01:11 -0400

    openXDA: Version change for build v3.0.5.113-master.

--- Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
+++ Source/Tools/DeviceDefinitionsMigrator/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.5.112")]
-[assembly: AssemblyVersion("3.0.5.112")]
-[assembly: AssemblyFileVersion("3.0.5.112")]
+// [assembly: AssemblyVersion("3.0.5.113")]
+[assembly: AssemblyVersion("3.0.5.113")]
+[assembly: AssemblyFileVersion("3.0.5.113")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
