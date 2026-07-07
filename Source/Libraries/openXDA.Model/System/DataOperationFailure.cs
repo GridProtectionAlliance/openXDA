2523474852ab86cc1a1cf7e3b79dcf2f9bc23a83
StephenCWills <swills@gridprotectionalliance.org>
2026-07-07 16:12:15 -0400

    Truncate DataOperationFailure.Log to 1000 bytes

--- Source/Libraries/openXDA.Model/System/DataOperationFailure.cs
+++ Source/Libraries/openXDA.Model/System/DataOperationFailure.cs
@@ -22,6 +22,9 @@
 //******************************************************************************************************
 
 using System;
+using System.Linq;
+using System.Text;
+using GSF;
 using GSF.Data.Model;
 
 namespace openXDA.Model
@@ -40,11 +43,41 @@ namespace openXDA.Model
         [ParentKey(typeof(FileGroupAnalysisJob))]
         public int FileGroupAnalysisJobID { get; set; }
 
-        public string Log { get; set; }
+        public string Log
+        {
+            get => Log_;
+            set => Log_ = TruncateUTF8(value, 1000);
+        }
+        private string Log_ { get; set; }
 
         public string StackTrace { get; set; }
 
         public DateTime TimeOfFailure { get; set; }
+
+        private static string TruncateUTF8(string str, int maxLength)
+        {
+            if (Encoding.UTF8.GetMaxByteCount(str.Length) <= maxLength)
+                return str;
+
+            if (Encoding.UTF8.GetByteCount(str) <= maxLength)
+                return str;
+
+            // If the first 1000 characters are all in the ASCII range,
+            // there are no multibyte characters in the string
+            // so the number of characters is the same as the number of bytes
+            if (str.Take(1000).All(c => c <= 0x7F))
+                return str.TruncateRight(maxLength);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            int end = maxLength;
+
+            // Scan backwards looking for the first byte
+            // of the first character beyond maxLength
+            while ((bytes[end] & 0xC0) == 0x80)
+                end--;
+
+            return Encoding.UTF8.GetString(bytes, 0, end);
+        }
     }
 
     [CustomView(@"
