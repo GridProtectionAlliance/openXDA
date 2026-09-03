94d2b1e44ce869c85db5f8385dd05048d49507ca
Erika Wills <elwills@gridprotectionalliance.org>
2026-09-03 17:02:23 -0400

    Remove depricated tables.

--- Source/Data/11 - SEBrowser.sql
+++ Source/Data/11 - SEBrowser.sql
@@ -176,55 +176,6 @@ GO
 Insert into ValueList (GroupID, Value, AltValue, SortOrder) VALUES((SELECT ID FROM ValueListGroup WHERE name = 'CustomReports'), 'CapBank Report', 'capbankreport',3)
 GO
 
-
-CREATE TABLE [SEBrowser.EventPreviewPaneSetting](
-	ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	Name varchar(200) NOT NULL,
-	Show bit NOT NULL DEFAULT (1),
-	OrderBy int NOT NULL
-)
-
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchOpenSEE', 1,1)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchFaultSegments', 1,2)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchAssetVoltageDisturbances', 1,3)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchCorrelatedSags', 1,4)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('TVAESRIMap', 0,5)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchFileInfo', 0,6)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchHistory', 1,7)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchRelayPerformance', 0,8)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchBreakerPerformance', 0,9)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchNoteWindow', 1,11)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('TVALightning', 0,12)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('TVAFaultInfo', 1,13)
-GO
-INSERT [SEBrowser.EventPreviewPaneSetting] (Name, Show, OrderBy) VALUES ('EventSearchCapBankAnalyticOverview', 0,10)
-GO
-
-CREATE TABLE [SEBrowser.Links] (
-	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	Name varchar(100) NOT NULL UNIQUE,
-	Display varchar(100) NOT NULL,
-	Value varchar(max) NOT NULL
-)
-GO
-
-INSERT INTO [SEBrowser.Links] (Name, Display,Value) VALUES
-	('Breaker Report',0,'breakerreport'),
-	('TCE  Report',0,'relayreport'),
-	('CapBank Report',0,'capbankreport')
-GO
-
 CREATE TABLE [SEBrowser.WidgetCategory] (
     ID INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
     Name varchar(50) NOT NULL,
